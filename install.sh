29d6d399bbf4520b8b2e39ac55b53c8c85fe029a
Truman <10185732+trumandk@users.noreply.github.com>
2021-01-11 12:26:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/debian  $(lsb_release -cs) stable"
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 apt-get install -y docker-compose build-essential firefox-esr
